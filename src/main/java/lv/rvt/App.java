2d5bcf862fc99cc0600484acd1d18a6b61bba773
23DP1ELevsa <166118110+23DP1ELevsa@users.noreply.github.com>
2024-09-17 08:11:49 +0000

    Speeding Ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,18 +5,13 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
-        {
-            Scanner scanner = new Scanner(System.in);
-            
-            System.out.println("Ievadi savu vārdu: ");
-            String name = scanner.nextLine();
-            
-            System.out.println("Ievadi savu uzvārdu: ");
-            String surname = scanner.nextLine();
+    {
+       Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Ievadi savas grupas nosaukumu: ");
-            String group = scanner.nextLine();
-
-            System.out.println("Students: " + name + " " + surname + ", grupa: " + group);
-        }
+       System.out.println("Give speed:");
+       int speed = Integer.valueOf(scanner.nextLine());
+       if (speed > 120) {
+        System.out.println("Speeding ticket!");
+       }
     }
+}
