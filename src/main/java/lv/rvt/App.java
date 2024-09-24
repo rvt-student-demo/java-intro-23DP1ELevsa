8f9fa271a6d854427c0cb1654d2a9172fa50de6f
23DP1ELevsa <166118110+23DP1ELevsa@users.noreply.github.com>
2024-09-24 09:58:07 +0000

    Sum of a Sequence

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,8 +7,13 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        for (int number = Integer.valueOf(scanner.nextLine()); number <= 100; number++) {
-            System.out.println(number);
+        int lastNumber = Integer.valueOf(scanner.nextLine());
+        int sum = 0;
+        int number = 1;
+        while (number <= lastNumber) {
+            sum += number;
+            number ++;
         }
+        System.out.println(sum);
     }
 }
