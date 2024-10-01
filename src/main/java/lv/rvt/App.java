46fbde28dac7f297ab85a5efa9bbf5580b30bc0b
23DP1ELevsa <166118110+23DP1ELevsa@users.noreply.github.com>
2024-10-01 06:36:21 +0000

    Repeating, Breaking and Remembering

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,13 +7,39 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        int lastNumber = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give numbers:");
         int sum = 0;
-        int number = 1;
-        while (number <= lastNumber) {
-            sum += number;
-            number ++;
+        int numbers = 0;
+        int even = 0;
+        int odd = 0;
+
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input < 0) {
+                System.out.println("Thx! Bye!");
+                break;
+            }
+
+            if (input >= 0) {
+                sum += input;
+                numbers++;
+            }
+
+            if (input % 2 == 1) {
+                odd++;
+            }
+
+            if (input % 2 == 0) {
+                even++;
+            }
         }
-        System.out.println(sum);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + numbers);
+        System.out.println("Average: " + (sum * 1.0) / numbers);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
     }
 }
