e77a61d3cba8acb1dc609d3b67ce0ab800cb8737
23DP1ELevsa <166118110+23DP1ELevsa@users.noreply.github.com>
2024-10-08 06:18:32 +0000

    Printing Stars

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class App 
 {
-    public static void main( String[] args ) {
-        divisibleByThreeInRange(2, 10);
-    }
-
-    public static void divisibleByThreeInRange(int beginning, int end) {
-        while (beginning <= end) {
-            if (beginning % 3 == 0) {
-                System.out.println(beginning);
-            }
-            beginning++;
+    public static void printStars(int number) {
+        int num = 0;
+        while (num < number) {
+            System.out.print("*");
+            num++;
         }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
     }
 }
