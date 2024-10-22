817554efb3f942c88695501dbce6e5ef3cc5027a
23DP1ELevsa <166118110+23DP1ELevsa@users.noreply.github.com>
2024-10-22 07:14:51 +0000

    Only These Numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,20 +7,31 @@ public class App
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList<Integer> intList = new ArrayList<>();
-        while (true) {
-            int input = Integer.valueOf(scanner.nextLine());
-            intList.add(input);
-            if (input == 0) {
-                break;
-            }
+        ArrayList<Integer> numberList = new ArrayList<>();
+        
+        numberList.add(Integer.valueOf(scanner.nextLine()));
+
+        while (numberList.get(numberList.size() - 1) != -1) {
+            numberList.add(Integer.valueOf(scanner.nextLine()));
+        } 
+        
+        System.out.print("From where? ");
+        int from = Integer.valueOf(scanner.nextLine());
+
+        while (numberList.size() - 1 < from || from < 0) {
+            from = Integer.valueOf(scanner.nextLine());
         }
 
-        // saskaitam visu elementu summu, kas atrodas ArrayListā
-        int listSum = 0;
-        for (int i = 0; i < intList.size(); i++) {
-            listSum += intList.get(i);
+        System.out.print("To where? ");
+        int to = Integer.valueOf(scanner.nextLine());
+
+        while (numberList.size() - 1 < to || to < 0) {
+            to = Integer.valueOf(scanner.nextLine());
         }
-        System.out.println(listSum);
+
+        while (from <= to) {
+            System.out.println(numberList.get(from));
+            from ++;
+        }
     }   
 }
