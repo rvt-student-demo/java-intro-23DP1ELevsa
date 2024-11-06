6b16024b72a072b6c5cd37faf0e783b471d8722a
23DP1ELevsa <166118110+23DP1ELevsa@users.noreply.github.com>
2024-11-06 13:56:08 +0000

    Count (Part 1)

--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -3,9 +3,13 @@ package lv.rvt;
 public class Person {
     private String name;
     private int age;
+    private int weight;
+    private int height;
 
     public Person(String initialName) {
         this.age = 0;
+        this.weight = 0;
+        this.height = 0;
         this.name = initialName;
     }
     
@@ -33,5 +37,18 @@ public class Person {
     
     public void printPerson() {
         System.out.println(this.name + ", age " + this.age + " years");
+    }
+
+    public void setHeight(int newHeight) {
+        this.height = newHeight;
+    }
+
+    public void setWeight(int newWeight) {
+        this.weight = newWeight;
+    }
+
+    public double bodyMassIndex() {
+        double heigthPerHundred = this.height / 100.0;
+        return this.weight / (heigthPerHundred * heigthPerHundred);
     }
 }
