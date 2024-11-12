e4127e585a1cffd9eb2cf0e476ede7b47d4aaaab
23DP1ELevsa <166118110+23DP1ELevsa@users.noreply.github.com>
2024-11-12 07:14:49 +0000

    Making Transactions (Part 2)

--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -7,6 +7,18 @@ public class PaymentCard {
         this.balance = openingBalance;
     }
 
+    public void eatAffordably() {
+        if (balance >= 2.60) {
+            balance -= 2.60;
+        }
+    }
+
+    public void eatHeartily() {
+        if (balance >= 4.60) {
+            balance -= 4.60;
+        }
+    }
+
     public String toString() {
         return "The card has a balance of " + balance + " euros";
     }
