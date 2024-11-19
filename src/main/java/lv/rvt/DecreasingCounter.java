aaed49f4bfa04c1255e84a94bea866d1d0b64982
23DP1ELevsa <166118110+23DP1ELevsa@users.noreply.github.com>
2024-11-19 10:21:53 +0000

    Overloaded Counter (2 parts)

--- src/main/java/lv/rvt/DecreasingCounter.java
+++ src/main/java/lv/rvt/DecreasingCounter.java
@@ -7,17 +7,39 @@ public class DecreasingCounter {
         this.value = initialValue;
     }
 
+    public void reset() {
+        this.value = 0;
+    }
+
+    public void Counter() {
+        this.value = 0;
+    }
+
     public void printValue() {
         System.out.println("value: " + this.value);
     }
 
+    public int value() {
+        return this.value;
+    }
+
+    public void increase() {
+        this.value ++;
+    }
+
     public void decrement() {
-        if (this.value > 0) {
-            this.value --;
+        this.value --;
+    }
+
+    public void increase(int increaseBy) {
+        if (increaseBy > 0) {
+            this.value += increaseBy;
         }
     }
 
-    public void reset() {
-        this.value = 0;
+    public void decrease(int decreaseBy) {
+        if (decreaseBy > 0) {
+            this.value -= decreaseBy;
+        }
     }
 }
--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -23,12 +23,6 @@ public class Person {
     public Person() {
 
     }
-    
-    public void growOlder() {   
-        if (this.age < 30) {
-            this.age ++;
-        }                                                                                                                                   
-    }
 
     public int returnAge() {
         return this.age;
@@ -65,4 +59,11 @@ public class Person {
         return this.weight / (heigthPerHundred * heigthPerHundred);
     }
     
+    public void growOlder() {
+        this.age = this.age + 1;
+    }
+    
+    public void growOlder(int years) {
+        this.age = this.age + years;
+    }
 }
