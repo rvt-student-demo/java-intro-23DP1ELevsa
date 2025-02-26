7b8b66b86d8b2937f4d93b41713e8bd5e9bd661a
23DP1ELevsa <166118110+23DP1ELevsa@users.noreply.github.com>
2025-02-26 07:51:59 +0000

    Money (Part 3)

--- src/main/java/lv/rvt/Money.java
+++ src/main/java/lv/rvt/Money.java
@@ -28,23 +28,45 @@ public class Money {
     }
 
     public Money plus(Money addition) {
-        int newEuros = this.euros + addition.euros;
-        int newCents = this.cents + addition.cents;
+        int plusEuros = this.euros + addition.euros;
+        int plusCents = this.cents + addition.cents;
 
-        if (newCents >= 100) {
-            newEuros += newCents / 100;
-            newCents = newCents % 100;
+        if (plusCents >= 100) {
+            plusEuros += plusCents / 100;
+            plusCents = plusCents % 100;
         }
 
+        return new Money(plusEuros, plusCents);
+    }
+
+    public Money minus(Money decreaser) {
+        int newEuros = this.euros - decreaser.euros;
+        int newCents = this.cents - decreaser.cents;
+    
+        if (newCents < 0) {
+            newEuros -= 1;
+            newCents += 100;
+        }
+    
+        if (newEuros < 0) {
+            return new Money(0, 0);
+        }
+    
         return new Money(newEuros, newCents);
     }
 
     public boolean lessThan(Money compared) {
         if (this.euros < compared.euros) {
             return true;
-        } else if (this.euros == compared.euros && this.cents < compared.cents) {
-            return true;
         }
+        
+        if (this.euros == compared.euros) {
+            if (this.cents < compared.cents) {
+                return true;
+            }
+        }
+        
         return false;
     }
+    
 }
